3f2c1fa97bc77582fb8284104101ad6f2a82b1d4
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-05-30 20:09:36 -0400

    added tree, set, hash table

--- dataStructure-prac.js
+++ dataStructure-prac.js
@@ -131,3 +131,388 @@ console.log(q.isEmpty());
 console.log(q.size());
 
 q.priorityEnqueue("test", 2);
+
+//linked list
+
+//node
+function Node(element) {
+  //node data
+  this.element = element;
+
+  // pointer to the next node
+  this.next = null;
+}
+
+function linkedList() {
+  let length = 0;
+  let head = null;
+
+  //return size
+  this.size = () => {
+    return length;
+  };
+
+  //return head
+  this.head = () => {
+    return head;
+  };
+
+  //
+  this.add = (element) => {
+    var node = new Node(element);
+    if (head == null) {
+      head = node;
+    } else {
+      var currentNode = head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = node;
+    }
+    length++;
+  };
+
+  //
+  this.remove = (element) => {
+    var currentNode = head;
+    var previousNode;
+    if (currentNode.element === element) {
+      head = currentNode.next;
+    } else {
+      while (currentNode.element !== element) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = currentNode.next;
+    }
+    length--;
+  };
+
+  // checks to see if the list is empty
+  this.isEmpty = function () {
+    return length === 0;
+  };
+
+  //
+  this.indexOf = function (element) {
+    var currentNode = head;
+    var index = -1;
+    while (currentNode) {
+      index++;
+      if (currentNode.element === element) {
+        return index;
+      }
+      currentNode = currentNode.next;
+    }
+    return index;
+  };
+
+  //returns the element held at an index
+  this.elementAt = function (index) {
+    var currentNode = head;
+    var count = 0;
+    while (count < index) {
+      count++;
+      currentNode = currentNode.next;
+    }
+    return currentNode.element;
+  };
+
+  //
+  this.addAt = function (index, element) {
+    var node = new Node(element);
+    var currentNode = head;
+    var previousNode;
+    var currentIndex = 0;
+    if (index > length) {
+      return false;
+    }
+    if (index === 0) {
+      node.next = currentNode;
+      head = node;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      node.next = currentNode;
+      previousNode.next = node;
+    }
+    length++;
+  };
+
+  //
+  this.removeAt = function (index) {
+    var currentNode = head;
+    var previousNode;
+    var currentIndex = 0;
+    if (index < 0 || index >= length) {
+      return null;
+    }
+    if (index === 0) {
+      head = currentIndex.next;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = currentNode.next;
+    }
+    length--;
+    return currentNode.element;
+  };
+}
+
+//set
+function MySet() {
+  var collection = [];
+  this.has = function (element) {
+    return collection.indexOf(element) !== -1;
+  };
+  this.values = function () {
+    return collection;
+  };
+  this.size = function () {
+    return collection.length;
+  };
+  this.add = function (element) {
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
+    }
+    return false;
+  };
+  this.remove = function (element) {
+    if (this.has(element)) {
+      index = collection.indexOf(element);
+      collection.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
+  this.union = function (otherSet) {
+    var unionSet = new MySet();
+    var firstSet = this.values();
+    var secondSet = otherSet.values();
+    firstSet.forEach(function (e) {
+      unionSet.add(e);
+    });
+    secondSet.forEach(function (e) {
+      unionSet.add(e);
+    });
+    return unionSet;
+  };
+  this.intersection = function (otherSet) {
+    var intersectionSet = new MySet();
+    var firstSet = this.values();
+    firstSet.forEach(function (e) {
+      if (otherSet.has(e)) {
+        intersectionSet.add(e);
+      }
+    });
+    return intersectionSet;
+  };
+  this.difference = function (otherSet) {
+    var differenceSet = new MySet();
+    var firstSet = this.values();
+    firstSet.forEach(function (e) {
+      if (!otherSet.has(e)) {
+        differenceSet.add(e);
+      }
+    });
+    return differenceSet;
+  };
+  this.subset = function (otherSet) {
+    var firstSet = this.values();
+    return firstSet.every(function (value) {
+      return otherSet.has(value);
+    });
+  };
+}
+
+//hash table
+function hash(string, max) {
+  var hash = 0;
+  for (var i = 0; i < string.length; i++) {
+    hash += string.charCodeAt(i);
+  }
+  return hash % max;
+}
+
+function HashTable() {
+  let storage = [];
+  const storageLimit = 4;
+
+  this.add = function (key, value) {
+    var index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      storage[index] = [[key, value]];
+    } else {
+      var inserted = false;
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      if (inserted === false) {
+        storage[index].push([key, value]);
+      }
+    }
+  };
+
+  this.remove = function (key) {
+    var index = hash(key, storageLimit);
+    if (storage[index].length === 1 && storage[index][0][0] === key) {
+      delete storage[index];
+    } else {
+      for (var i = 0; i < storage[index]; i++) {
+        if (storage[index][i][0] === key) {
+          delete storage[index][i];
+        }
+      }
+    }
+  };
+
+  this.lookup = function (key) {
+    var index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          return storage[index][i][1];
+        }
+      }
+    }
+  };
+}
+
+//tree
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  add(data) {
+    const node = this.root;
+    if (node === null) {
+      this.root = new Node(data);
+      return;
+    } else {
+      const searchTree = function (node) {
+        if (data < node.data) {
+          if (node.left === null) {
+            node.left = new Node(data);
+            return;
+          } else if (node.left !== null) {
+            return searchTree(node.left);
+          }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            node.right = new Node(data);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
+        }
+      };
+      return searchTree(node);
+    }
+  }
+
+  findMin() {
+    let current = this.root;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
+  }
+
+  findMax() {
+    let current = this.root;
+    while (current.right !== null) {
+      current = current.right;
+    }
+    return current.data;
+  }
+
+  find(data) {
+    let current = this.root;
+    while (current.data !== data) {
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+      if (current === null) {
+        return null;
+      }
+    }
+    return current;
+  }
+
+  isPresent(data) {
+    let current = this.root;
+    while (current) {
+      if (data === current.data) {
+        return true;
+      }
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
+
+  remove(data) {
+    const removeNode = function (node, data) {
+      if (node == null) {
+        return null;
+      }
+      if (data == node.data) {
+        // no child node
+        if (node.left == null && node.right == null) {
+          return null;
+        }
+        // no left node
+        if (node.left == null) {
+          return node.right;
+        }
+        // no right node
+        if (node.right == null) {
+          return node.left;
+        }
+        // has 2 child nodes
+        var tempNode = node.right;
+        while (tempNode.left !== null) {
+          tempNode = tempNode.left;
+        }
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+    this.root = removeNode(this.root, data);
+  }
+}
